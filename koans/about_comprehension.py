b8d41e47f6efd12ded29fc02623009c2d2dac7be
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-07-20 23:01:30 +0200

    complete about_comprehension

--- koans/about_comprehension.py
+++ koans/about_comprehension.py
@@ -13,49 +13,51 @@ class AboutComprehension(Koan):
 
         comprehension = [delicacy.capitalize() for delicacy in feast]
 
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual("Lambs", comprehension[0])
+        self.assertEqual("Orangutans", comprehension[2])
 
     def test_filtering_lists_with_list_comprehensions(self):
         feast = ['spam', 'sloths', 'orangutans', 'breakfast cereals',
             'fruit bats']
 
-        comprehension = [delicacy for delicacy in feast if len(delicacy) > 6]
+        comprehension = [delicacy for delicacy in feast if len(delicacy) > 6] #looks like this is another way to filter. add item if item in array is len(item) > 6
 
-        self.assertEqual(__, len(feast))
-        self.assertEqual(__, len(comprehension))
+        self.assertEqual(5, len(feast))
+        self.assertEqual(3, len(comprehension))
 
     def test_unpacking_tuples_in_list_comprehensions(self):
         list_of_tuples = [(1, 'lumberjack'), (2, 'inquisition'), (4, 'spam')]
-        comprehension = [ skit * number for number, skit in list_of_tuples ]
+        comprehension = [ skit * number for number, skit in list_of_tuples ] #word * number for every number and word in the list
 
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
+        self.assertEqual("lumberjack", comprehension[0])
+        self.assertEqual("spamspamspamspam", comprehension[2])
 
     def test_double_list_comprehension(self):
         list_of_eggs = ['poached egg', 'fried egg']
         list_of_meats = ['lite spam', 'ham spam', 'fried spam']
 
 
-        comprehension = [ '{0} and {1}'.format(egg, meat) for egg in list_of_eggs for meat in list_of_meats]
+        comprehension = [ '{0} and {1}'.format(egg, meat) for egg in list_of_eggs for meat in list_of_meats] #iterates over items in eggs and meats are nested in every egg item
 
 
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, len(comprehension))
+        self.assertEqual("poached egg and lite spam", comprehension[0])
+        self.assertEqual(6, len(comprehension))
 
     def test_creating_a_set_with_set_comprehension(self):
         comprehension = { x for x in 'aabbbcccc'}
 
-        self.assertEqual(__, comprehension)  # remember that set members are unique
+        self.assertEqual({'a', 'b', 'c'}, comprehension)  # remember that set members are unique
 
     def test_creating_a_dictionary_with_dictionary_comprehension(self):
         dict_of_weapons = {'first': 'fear', 'second': 'surprise',
                            'third':'ruthless efficiency', 'fourth':'fanatical devotion',
                            'fifth': None}
 
-        dict_comprehension = { k.upper(): weapon for k, weapon in dict_of_weapons.items() if weapon}
+        dict_comprehension = { k.upper(): weapon for k, weapon in dict_of_weapons.items() if weapon} #all of the keys have been uppered
 
-        self.assertEqual(__, 'first' in dict_comprehension)
-        self.assertEqual(__, 'FIRST' in dict_comprehension)
-        self.assertEqual(__, len(dict_of_weapons))
-        self.assertEqual(__, len(dict_comprehension))
+        self.assertEqual(False, 'first' in dict_comprehension)
+        self.assertEqual(True, 'FIRST' in dict_comprehension)
+        self.assertEqual(5, len(dict_of_weapons))
+        self.assertEqual(4, len(dict_comprehension)) #because you can't upper None
+
+#and they say Python is readable. Worst unit so far.
